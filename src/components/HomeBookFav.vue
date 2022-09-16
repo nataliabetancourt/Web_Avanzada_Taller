54218d7b3571613554cd22577d91c49856f78d1d
Natalia Betancourt <nataliabetancourt@MacBook-Air-de-Natalia.local>
2022-09-16 14:34:46 -0500

    Home page additions for dinamic css

--- src/components/HomeBookFav.vue
+++ src/components/HomeBookFav.vue
@@ -1,155 +1,187 @@
 <script>
-    export default {
-        data() {
-            return {
-            titles: [
-                {
-                name: "TOP 100 BOOKS",
-                description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Nullam non nisi est sit amet facilisis magna etiam tempor. Tincidunt nunc pulvinar sapien et ligula ullamcorper malesuada. Aliquam sem fringilla ut morbi tincidunt augue interdum. Aliquam ultrices sagittis orci a scelerisque purus semper eget duis. ",
-                isShowing: false,
-                books: [
-                    'https://static.independent.co.uk/2022/08/08/19/Books-Q%26A-Jennette_McCurdy_52079.jpg?width=1200',
-                    'https://i0.wp.com/ellaberintodelminotauro.com.co/wp-content/uploads/2022/02/fairy-tale-9781668002179_xlg.jpg?ssl=1',
-                    'https://m.media-amazon.com/images/I/71PNGYHykrL.jpg',
-                    'https://images-na.ssl-images-amazon.com/images/I/91ajvO3vlnL.jpg',
-                    'https://images-na.ssl-images-amazon.com/images/I/61h3ZkQxjTL.jpg',
-                    'https://images-na.ssl-images-amazon.com/images/I/61tqfa+xbWL.jpg'
-                ]
-                },
-                {
-                name: "FALL'S BIGGEST BOOKS",
-                description:  "Ultrices mi tempus imperdiet nulla malesuada pellentesque. Vel eros donec ac odio tempor orci dapibus ultrices in. Nisl condimentum id venenatis a condimentum vitae sapien. Orci phasellus egestas tellus rutrum tellus pellentesque eu. Sapien nec sagittis aliquam malesuada bibendum arcu vitae elementum. Fermentum odio eu feugiat pretium nibh ipsum consequat nisl. Sodales ut eu sem integer vitae justo eget. Morbi enim nunc faucibus a pellentesque sit amet. ",
-                isShowing: false,
-                books: [
-                    'https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1647526587l/60415700.jpg',
-                    'https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1660752887l/59830251.jpg',
-                    'https://images-na.ssl-images-amazon.com/images/I/81m4rgPWCFL.jpg',
-                    'https://images.penguinrandomhouse.com/cover/9780593446065',
-                    'https://images-na.ssl-images-amazon.com/images/I/41-5B-pCIkL._SX329_BO1,204,203,200_.jpg',
-                    'https://images-na.ssl-images-amazon.com/images/I/81e29pejAeL.jpg'
-                ]
-                },
-                {
-                name: "YA FAVORITES",
-                description: "Tristique risus nec feugiat in fermentum posuere urna nec tincidunt. Eget gravida cum sociis natoque penatibus et magnis. Non arcu risus quis varius quam quisque id diam vel. Nulla aliquet enim tortor at auctor urna. Ac turpis egestas integer eget aliquet nibh. Dui faucibus in ornare quam. Parturient montes nascetur ridiculus mus mauris vitae ultricies leo integer.",
-                isShowing: false,
-                books: [
-                    'https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1633986670l/59227936.jpg',
-                    'https://images-na.ssl-images-amazon.com/images/I/81xSl05tjtL.jpg',
-                    'https://images-na.ssl-images-amazon.com/images/I/91Iuhbh7aIL.jpg',
-                    'https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1616690030i/56913189.jpg',
-                    'https://images-na.ssl-images-amazon.com/images/I/81VDhiKgyaL.jpg',
-                    'https://images-na.ssl-images-amazon.com/images/I/81896Wx2dUL.jpg'
-                ]
-                },
-            ],
-            showInfo: {},
-            };
+export default {
+  data() {
+    return {
+      titles: [
+        {
+          name: "TOP 100 BOOKS",
+          description:
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Nullam non nisi est sit amet facilisis magna etiam tempor. Tincidunt nunc pulvinar sapien et ligula ullamcorper malesuada. Aliquam sem fringilla ut morbi tincidunt augue interdum. Aliquam ultrices sagittis orci a scelerisque purus semper eget duis. ",
+          books: [
+            "https://static.independent.co.uk/2022/08/08/19/Books-Q%26A-Jennette_McCurdy_52079.jpg?width=1200",
+            "https://i0.wp.com/ellaberintodelminotauro.com.co/wp-content/uploads/2022/02/fairy-tale-9781668002179_xlg.jpg?ssl=1",
+            "https://m.media-amazon.com/images/I/71PNGYHykrL.jpg",
+            "https://images-na.ssl-images-amazon.com/images/I/91ajvO3vlnL.jpg",
+            "https://images-na.ssl-images-amazon.com/images/I/61h3ZkQxjTL.jpg",
+            "https://images-na.ssl-images-amazon.com/images/I/61tqfa+xbWL.jpg",
+          ],
+          title: 1,
+          isShowing: false,
         },
-
-        methods: {
-            example() {
-                console.log("it works");
-            },
-
-            onListItemClick(title) {
-                this.showInfo.name = title.name;
-                this.showInfo.description = title.description;
-                this.showInfo.books = title.books;
-                this.showInfo.showButton = true;
-            },
+        {
+          name: "FALL'S BIGGEST BOOKS",
+          description:
+            "Ultrices mi tempus imperdiet nulla malesuada pellentesque. Vel eros donec ac odio tempor orci dapibus ultrices in. Nisl condimentum id venenatis a condimentum vitae sapien. Orci phasellus egestas tellus rutrum tellus pellentesque eu. Sapien nec sagittis aliquam malesuada bibendum arcu vitae elementum. Fermentum odio eu feugiat pretium nibh ipsum consequat nisl. Sodales ut eu sem integer vitae justo eget. Morbi enim nunc faucibus a pellentesque sit amet. ",
+          books: [
+            "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1647526587l/60415700.jpg",
+            "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1660752887l/59830251.jpg",
+            "https://images-na.ssl-images-amazon.com/images/I/81m4rgPWCFL.jpg",
+            "https://images.penguinrandomhouse.com/cover/9780593446065",
+            "https://images-na.ssl-images-amazon.com/images/I/41-5B-pCIkL._SX329_BO1,204,203,200_.jpg",
+            "https://images-na.ssl-images-amazon.com/images/I/81e29pejAeL.jpg",
+          ],
+          title: 2,
+          isShowing: false,
         },
-    }
+        {
+          name: "YA FAVORITES",
+          description:
+            "Tristique risus nec feugiat in fermentum posuere urna nec tincidunt. Eget gravida cum sociis natoque penatibus et magnis. Non arcu risus quis varius quam quisque id diam vel. Nulla aliquet enim tortor at auctor urna. Ac turpis egestas integer eget aliquet nibh. Dui faucibus in ornare quam. Parturient montes nascetur ridiculus mus mauris vitae ultricies leo integer.",
+          books: [
+            "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1633986670l/59227936.jpg",
+            "https://images-na.ssl-images-amazon.com/images/I/81xSl05tjtL.jpg",
+            "https://images-na.ssl-images-amazon.com/images/I/91Iuhbh7aIL.jpg",
+            "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1616690030i/56913189.jpg",
+            "https://images-na.ssl-images-amazon.com/images/I/81VDhiKgyaL.jpg",
+            "https://images-na.ssl-images-amazon.com/images/I/81896Wx2dUL.jpg",
+          ],
+          title: 3,
+          isShowing: false,
+        },
+      ],
+      highlightTitle: 0,
+      showInfo: {},
+    };
+  },
+
+  methods: {
+    example() {
+      console.log("it works");
+    },
+
+    onListItemClick(title) {
+      //Establish which title is showing
+      this.showInfo.name = title.name;
+      this.showInfo.description = title.description;
+      this.showInfo.books = title.books;
+      this.showInfo.showButton = true;
+      this.highlightTitle = title.title;
+    },
+  },
+
+  watch: {
+    highlightTitle(val) {
+      for (let index = 0; index < this.titles.length; index++) {
+        //Check if selected title is the same and change color
+        if (val == this.titles[index].title) {
+          this.titles[index].isShowing = true;
+        } else {
+          this.titles[index].isShowing = false;
+        }
+      }
+    },
+  },
+};
 </script>
 
 <template>
   <ul>
     <li
-      class="favorites" :class="{'favorites--selected': title.isShowing}" v-for="title in titles" :key="title.name" @click="() => onListItemClick(title)"> {{ title.name }}
+      class="favorites"
+      :class="{ 'favorites--selected': title.isShowing }"
+      v-for="title in titles"
+      :key="title.name"
+      @click="() => onListItemClick(title)"
+    >
+      {{ title.name }}
     </li>
   </ul>
   <div class="info">
-    <p class="info__description">{{showInfo.description}}</p>
+    <p class="info__description">{{ showInfo.description }}</p>
     <div class="info__images">
-        <img class="info__images--img" v-for="book in showInfo.books" v-bind:src="book"/>
+      <img
+        class="info__images--img"
+        v-for="book in showInfo.books"
+        v-bind:src="book"
+      />
     </div>
     <button class="info__btn" v-if="showInfo.showButton">SEE MORE</button>
   </div>
 </template>
 
 <style scoped lang="scss">
-$background: #FDFAF3;
-$fontColor: #391B00;
-$mainColor: #6739CB;
+$background: #fdfaf3;
+$fontColor: #391b00;
+$mainColor: #6739cb;
 
 li {
-    list-style: none;
+  list-style: none;
 
-    &__hover {
-        cursor: pointer;
-    }
+  &__hover {
+    cursor: pointer;
+  }
 }
 
 ul {
-    display: flex;
-    margin: 130px 80px 0 80px;
-    justify-content: space-between;
-    cursor: pointer;
+  display: flex;
+  margin: 130px 80px 0 80px;
+  justify-content: space-between;
+  cursor: pointer;
 }
 .favorites {
-    font-family: 'Outfit', sans-serif;
-    font-weight: 600;
-    font-size: 1.4em;
-    color: $fontColor;
+  font-family: "Outfit", sans-serif;
+  font-weight: 600;
+  font-size: 1.4em;
+  color: $fontColor;
 
-    &--selected {
-        font-weight: 900;
-        color: $mainColor;
-    }
+  &--selected {
+    font-weight: 900;
+    color: $mainColor;
+  }
 }
 
 .info {
-    margin: 40px 45px 20px 45px;
+  margin: 40px 45px 20px 45px;
 
-    &__description {
-        color: $fontColor;
-        margin: auto;
-        font-family: 'Outfit', sans-serif;
-        font-size: 1em;
-        font-weight: 300;
-        text-align: center;
-        width: 70%;
-    }
+  &__description {
+    color: $fontColor;
+    margin: auto;
+    font-family: "Outfit", sans-serif;
+    font-size: 1em;
+    font-weight: 300;
+    text-align: center;
+    width: 70%;
+  }
 
-    &__images {
-        display: flex;
-        &--img {
-            width: 20%;
-            height: 240px;
-            object-fit: contain;
-            margin: 30px 0 30px 0;
-        } 
+  &__images {
+    display: flex;
+    &--img {
+      width: 20%;
+      height: 240px;
+      object-fit: contain;
+      margin: 30px 0 30px 0;
     }
+  }
 
-    &__btn {
-        display: flex;
-        justify-content: center;
-        text-align: center;
-        line-height: 38px;
-        margin: auto;
-        width: 50%;
-        height: 40px;
-        font-family: 'Outfit', sans-serif;
-        font-size: 1em;
-        margin-top: 20px 0 80px;
-        border: #391B00 solid 2px;
-        background-color: transparent;
-    
-        &:hover {
-          background-color: $mainColor;
-          font-weight: 600;
-          color: $background;
-        }
+  &__btn {
+    display: flex;
+    justify-content: center;
+    text-align: center;
+    line-height: 38px;
+    margin: auto;
+    width: 50%;
+    height: 40px;
+    font-family: "Outfit", sans-serif;
+    font-size: 1em;
+    margin-top: 20px 0 80px;
+    border: #391b00 solid 2px;
+    background-color: transparent;
+
+    &:hover {
+      background-color: $mainColor;
+      font-weight: 600;
+      color: $background;
     }
+  }
 }
 </style>
--- src/views/AddProductView.vue
+++ src/views/AddProductView.vue
@@ -1,133 +1,159 @@
 <template>
-        <h2 class="title">Add product</h2>
-        <form class="form">
-            <label for="name" class="form__label">BOOK TITLE</label>
-            <input type="text" name="name" placeholder="Book title" class="form__input" id="title">
-            
-            <label for="name" class="form__label">BOOK AUTHOR</label>
-            <input type="text" name="name" placeholder="Book author" class="form__input" id="author">
+  <h2 class="title">Add product</h2>
+  <form class="form">
+    <label for="name" class="form__label">BOOK TITLE</label>
+    <input
+      type="text"
+      name="name"
+      placeholder="Book title"
+      class="form__input"
+      id="title"
+    />
 
-            <label for="price" class="form__label">PRICE</label>
-            <input type="number" name="price" placeholder="Book price" class="form__input" id="price">
+    <label for="name" class="form__label">BOOK AUTHOR</label>
+    <input
+      type="text"
+      name="name"
+      placeholder="Book author"
+      class="form__input"
+      id="author"
+    />
 
-            <label for="description" class="form__label">DESCRIPTION</label>
-            <textarea name="description" class="form__input form__input--description" id="description" placeholder="Write the book summary here..."></textarea>
+    <label for="price" class="form__label">PRICE</label>
+    <input
+      type="number"
+      name="price"
+      placeholder="Book price"
+      class="form__input"
+      id="price"
+    />
 
-            <label for="category" class="form__label">GENRE</label>
-            <select name="category" id="category" class="form__input">
-                <option hidden disabled selected value>Select an option... </option>
-                <option value="face">Classics</option>
-                <option value="cheek">Horror</option>
-                <option value="eyes">Thriller</option>
-                <option value="lips">Romance</option>
-                <option value="lips">Sci-Fi</option>
-                <option value="lips">Fantasy</option>
-            </select>
-                        
-            <div class="form__row">
-                <label for="images" class="form__label">IMAGES</label>
-                <input type="file" name="images" id="images" class="form__upload" multiple>
-    
-                <input type="submit" value="ADD PRODUCT" class="form__submit">
-            </div>
-        </form>
-        <img class="illustration" src="../assets/illustration.png" alt="">
+    <label for="description" class="form__label">DESCRIPTION</label>
+    <textarea
+      name="description"
+      class="form__input form__input--description"
+      id="description"
+      placeholder="Write the book summary here..."
+    ></textarea>
+
+    <label for="category" class="form__label">GENRE</label>
+    <select name="category" id="category" class="form__input">
+      <option hidden disabled selected value>Select an option...</option>
+      <option value="face">Classics</option>
+      <option value="cheek">Horror</option>
+      <option value="eyes">Thriller</option>
+      <option value="lips">Romance</option>
+      <option value="lips">Sci-Fi</option>
+      <option value="lips">Fantasy</option>
+    </select>
+
+    <div class="form__row">
+      <label for="images" class="form__label">IMAGES</label>
+      <input
+        type="file"
+        name="images"
+        id="images"
+        class="form__upload"
+        multiple
+      />
+
+      <input type="submit" value="ADD PRODUCT" class="form__submit" />
+    </div>
+  </form>
+  <img class="illustration" src="../assets/illustration.png" alt="" />
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
 <style lang="scss">
-$background: #FDFAF3;
-$fontColor: #391B00;
-$mainColor: #6739CB;
+$background: #fdfaf3;
+$fontColor: #391b00;
+$mainColor: #6739cb;
 
 .title {
-    margin: 100px 0 0 45px;
-    font-family: 'Outfit', sans-serif;
-    font-weight: 600;
-    font-size: 1.6em;
+  margin: 100px 0 0 45px;
+  font-family: "Outfit", sans-serif;
+  font-weight: 600;
+  font-size: 1.6em;
 }
 
 .form {
-    margin: 30px 45px;
-    font-family: 'Outfit', sans-serif;
+  margin: 30px 45px;
+  font-family: "Outfit", sans-serif;
 
-    &__label {
-        font-weight: 500;
-        font-size: 1em;
+  &__label {
+    font-weight: 500;
+    font-size: 1em;
+  }
+
+  &__input {
+    font-family: "Outfit", sans-serif;
+    display: block;
+    font-size: 1em;
+    border: solid 2px $mainColor;
+    padding: 5px;
+    width: 50%;
+    height: 25px;
+    margin-top: 5px;
+    margin-bottom: 18px;
+
+    &--description {
+      height: 70px;
+    }
+  }
+
+  &__upload {
+    display: block;
+    margin: 8px 0;
+
+    &::-webkit-file-upload-button {
+      visibility: hidden;
     }
 
-    &__input {
-        font-family: 'Outfit', sans-serif;
-        display: block;
-        font-size: 1em;
-        border: solid 2px $mainColor;
-        padding: 5px;
-        width: 50%;
-        height: 25px;
-        margin-top: 5px;
-        margin-bottom: 18px;
-
-        &--description {
-            height: 70px;
-        }
+    &::before {
+      content: "Upload book cover";
+      color: $fontColor;
+      display: inline-block;
+      background-color: white;
+      border: 2px solid $mainColor;
+      border-radius: 3px;
+      padding: 5px 8px;
+      outline: none;
+      cursor: pointer;
+      font-weight: 700;
     }
 
-    &__upload {
-        display: block;
-        margin: 8px 0;
-
-        &::-webkit-file-upload-button {
-            visibility: hidden;
-        }
-
-        &::before {
-            content: 'Upload book cover';
-            color: $fontColor;
-            display: inline-block;
-            background-color: white;
-            border: 2px solid $mainColor;
-            border-radius: 3px;
-            padding: 5px 8px;
-            outline: none;
-            cursor: pointer;
-            font-weight: 700;
-        }
-
-        &:hover::before {
-            background: $mainColor;
-            color: $background;
-        }
+    &:hover::before {
+      background: $mainColor;
+      color: $background;
     }
+  }
 
-    &__submit {
-        font-family: 'Outfit', sans-serif;
-        color: $fontColor;
-        background-color: transparent;
-        border: 2px solid $mainColor;
-        padding: 10px 40px;
-        width: 35%;
-        margin-top: 20px;
-        font-weight: 500;
+  &__submit {
+    font-family: "Outfit", sans-serif;
+    color: $fontColor;
+    background-color: transparent;
+    border: 2px solid $mainColor;
+    padding: 10px 40px;
+    width: 35%;
+    margin-top: 20px;
+    font-weight: 500;
 
-        &:hover {
-            border: 2px solid $fontColor;
-            background-color: $fontColor;
-            font-weight: normal;
-            color: $background;
-        }
+    &:hover {
+      border: 2px solid $fontColor;
+      background-color: $fontColor;
+      font-weight: normal;
+      color: $background;
     }
+  }
 }
 
 .illustration {
-    position: absolute;
-    right: 0;
-    width: 40%;
-    top: 120px;
+  position: absolute;
+  right: 0;
+  width: 40%;
+  top: 120px;
 }
-
 </style>
